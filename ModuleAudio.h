4159383a5b97b08e386a79f54431f7cafc2794d1
Rafa López <35458745+Rengrow@users.noreply.github.com>
2019-03-31 21:24:25 +0200

    Revert "FadeOutMusic en FadeBlackScreen"
    This reverts commit 2aac1555af29f1a1eff87c8f93880fac1ceb5c6a.

---
+++ ModuleAudio.h
@@ -0,0 +1,37 @@
+#ifndef __ModuleAudio_H__
+#define __ModuleAudio_H__
+
+#include "Module.h"
+#include "Globals.h"
+#include "SDL_Mixer/include/SDL_mixer.h"
+
+#define MAX_SONGS 50
+#define MAX_CHUNKS 50
+
+class ModuleAudio : public Module
+{
+public:
+
+	ModuleAudio();
+	~ModuleAudio();
+
+	bool Init();
+	bool CleanUp();
+
+	Mix_Chunk * const LoadChunk(const char* path);
+	Mix_Music * const LoadSong(const char* path);
+
+	bool Unload(Mix_Music * song);
+	bool Unload(Mix_Chunk * chunk);
+
+	bool const PlaySong(const int song);
+	bool const PlaySongDelay(const int song, int loops, int ms);
+	bool const PlayChunk(const int chunk);
+
+public:
+	Mix_Music * songs[MAX_SONGS];
+	Mix_Chunk * chunks[MAX_CHUNKS];
+
+};
+
+#endif // __ModuleAudio_H__
---
+++ ModulePlayer.h
@@ -0,0 +1,37 @@
+#ifndef __ModulePlayer_H__
+#define __ModulePlayer_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+
+class ModulePlayer : public Module
+{
+public:
+	ModulePlayer();
+	~ModulePlayer();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+
+	SDL_Texture* graphics = nullptr;
+	Animation idle;
+	Animation forward;
+	Animation backward;
+	Animation lp, lk;
+	Animation neutralJump;
+	iPoint position;
+	bool atacar = false;
+	bool jump = false;
+	int framesAtaque = 0;
+	int framesJump = 0;
+	int mov; //lp, mp, hp, lk, mk, hk
+};
+
+#endif
