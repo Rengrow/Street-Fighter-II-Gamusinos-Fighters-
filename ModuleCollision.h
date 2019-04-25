853dfe73354282dbc02c3ba74dfd6aabbc1d4c38
Rafa López <35458745+Rengrow@users.noreply.github.com>
2019-04-25 17:57:31 +0200

    Repair CheckCollisions

--- ModuleCollision.h
+++ ModuleCollision.h
@@ -55,11 +55,14 @@ public:
 
 	update_status PreUpdate() override;
 	update_status Update() override;
+	update_status PostUpdate() override;
 	bool CleanUp() override;
 
 	Collider* AddCollider(SDL_Rect rect, COLLIDER_TYPE type, Module* callback = nullptr);
 	Collider* AddCollider(Collider collider);
 	void DebugDraw();
+	void RemoveDeletedColliders();
+	void CalculateCollisions();
 
 private:
 
