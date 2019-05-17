b3375b4f19c4970578f328b2332a107f2c508da4
Rafa López <35458745+Rengrow@users.noreply.github.com>
2019-05-17 09:49:32 +0200

    Add Module Slowdown and camera shake

--- ModuleRender.h
+++ ModuleRender.h
@@ -22,6 +22,9 @@ public:
 	
 	bool Blit(SDL_Texture* texture, int x, int y, SDL_Rect* section, bool flip, float speed = 1.0f);
 	bool DrawQuad(const SDL_Rect& rect, Uint8 r, Uint8 g, Uint8 b, Uint8 a, bool use_camera = true);
+
+	void StartCameraShake(int duration, float magnitude);
+	void UpdateCameraShake();
 	
 public:
 	SDL_Renderer* renderer = nullptr;
@@ -30,6 +33,15 @@ public:
 	SDL_Rect limit2Box;
 	int cameraSpeed = 1;  //this used to be 2, but caused players to vibrate
 	int scenelimit;
+
+private:
+	bool shaking = false;
+
+	int shake_duration = 0;
+	int shake_timer = 0;
+	int shake_magnitude = 0;
+
+	SDL_Point camera_offset;
 	
 };
 
